b511b9ea66b1a7a75484340da06adf9f525d45a4
M_788192 <m_788192@ambus54920.home>
2021-01-12 18:39:04 -0600

    mocked current repo layer in IT test

--- src/test/java/com/galvanize/heroapp/it/HeroControllerIT.java
+++ src/test/java/com/galvanize/heroapp/it/HeroControllerIT.java
@@ -1,5 +1,6 @@
 package com.galvanize.heroapp.it;
 
+import com.galvanize.heroapp.repository.HeroRepository;
 import com.galvanize.heroapp.service.HeroService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+
 import static java.util.Arrays.asList;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -24,19 +28,25 @@ public class HeroControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private HeroRepository heroRepository;
+
 
     @Test
     void getAllHeroNames_return204IfEmpty() throws Exception {
-
+        when(heroRepository.findAll()).thenReturn(new ArrayList<>());
         mockMvc.perform(get("/hero"))
                 .andExpect(status().isNoContent());
     }
 
     @Test
     public void getAllHeroNames_returnListOfNames() throws Exception {
-
+        when(heroRepository.findAll()).thenReturn(asList("test1", "test2"));
         mockMvc.perform(get("/hero"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0]").value("test1"))
+                .andExpect(jsonPath("$[1]").value("test2"));
     }
 
 }
